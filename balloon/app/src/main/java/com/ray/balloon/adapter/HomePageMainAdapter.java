de6646e204a904a7f22cb725ed6ed608c214a456
Administrator <Administrator>
2016-03-23 10:43:06 +0800

    Signed-off-by: ray

--- balloon/app/src/main/java/com/ray/balloon/adapter/HomePageMainAdapter.java
+++ balloon/app/src/main/java/com/ray/balloon/adapter/HomePageMainAdapter.java
@@ -7,11 +7,13 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
 import com.corelibs.utils.DisplayUtil;
-import com.corelibs.utils.ViewHolder;
+import com.corelibs.utils.ToastMgr;
 import com.corelibs.views.NoScrollingGridView;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.ray.balloon.R;
@@ -51,39 +53,101 @@ public class HomePageMainAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
+        Holder holder = null;
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.item_home_page, null);
+            holder = new Holder();
+            ViewStub linkOrImgViewStub = (ViewStub) view.findViewById(R.id.linkOrImgViewStub);
+            switch (position % 2) {
+                case 0:// 图片
+                    linkOrImgViewStub.setLayoutResource(R.layout.viewstub_image);
+                    linkOrImgViewStub.inflate();
+                    LinearLayout ll_image_view = (LinearLayout) view.findViewById(R.id.ll_image_view);
+                    if (ll_image_view != null) {
+                        holder.ll_image_view = ll_image_view;
+                        holder.gv_images = (NoScrollingGridView) ll_image_view.findViewById(R.id.gv_images);
+                    }
+                    break;
+                case 1:
+                    linkOrImgViewStub.setLayoutResource(R.layout.viewstub_video);
+                    linkOrImgViewStub.inflate();
+                    LinearLayout ll_video = (LinearLayout) view.findViewById(R.id.ll_video);
+                    if (ll_video != null) {
+                        holder.ll_video = ll_video;
+                        holder.item_video_shoot = (SimpleDraweeView) ll_video.findViewById(R.id.item_video_shoot);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            holder.card = (CardView) view.findViewById(R.id.card_main);
+            holder.item_preson_icon = (SimpleDraweeView) view.findViewById(R.id.item_preson_icon);
+            holder.mtv_content = (MTextView) view.findViewById(R.id.mtv_content);
+            view.setTag(holder);
+        } else {
+            holder = (Holder) view.getTag();
         }
-        CardView card = ViewHolder.get(view, R.id.card_main);
 
-        card.setClickable(true);
-        card.setBackgroundColor(Color.WHITE);
+        holder.card.setClickable(true);
+        holder.card.setBackgroundColor(Color.WHITE);
 
         RippleDrawable rippleDrawable = new RippleDrawableFroyo(ColorStateList.valueOf(0x42ff0000), null, RippleDrawable.Style.Over);
-        rippleDrawable.setCallback(card);
+        rippleDrawable.setCallback(holder.card);
         rippleDrawable.setHotspotEnabled(true);
-        card.setRippleDrawable(rippleDrawable);
+        holder.card.setRippleDrawable(rippleDrawable);
 
-        SimpleDraweeView item_preson_icon = ViewHolder.get(view, R.id.item_preson_icon);
-        item_preson_icon.setImageURI(Uri.parse("http://imgsrc.baidu.com/forum/pic/item/3b292df5e0fe992503d986c834a85edf8cb17155.jpg"));
 
-        MTextView mtv_content = ViewHolder.get(view, R.id.mtv_content);
-        MTextViewUtils.setText(mtv_content, "默认使用一个 shader 绘制圆角，但是仅仅占位图和所要显示的图有圆角效果。失败示意图和重下载示意图无圆角效果，且这种圆角方式不支持动画", context);
+        holder.item_preson_icon.setImageURI(Uri.parse("http://imgsrc.baidu.com/forum/pic/item/3b292df5e0fe992503d986c834a85edf8cb17155.jpg"));
 
-        NoScrollingGridView gv_images = ViewHolder.get(view, R.id.gv_images);
-        HomePageImagesAdapter childAdapter = new HomePageImagesAdapter(context);
-        gv_images.setAdapter(childAdapter);
-        gv_images.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // 因为单张图片时，图片实际大小是自适应的，imageLoader缓存时是按测量尺寸缓存的
-                DisplayUtil.getNetImageWidthBIHeight();
-                HomePageImagesAdapter adapter= (HomePageImagesAdapter) parent.getAdapter();
+        MTextViewUtils.setText(holder.mtv_content, "默认使用一个 shader 绘制圆角，但是仅仅占位图和所要显示的图有圆角效果。失败示意图和重下载示意图无圆角效果，且这种圆角方式不支持动画", context);
 
-                ImagePagerActivity.startImagePagerActivity(context, adapter.getPhones(), position);
-            }
-        });
+
+        switch (position % 2) {
+            case 0:
+                if (holder.ll_image_view != null) {
+                    holder.ll_image_view.setVisibility(View.VISIBLE);
+                    HomePageImagesAdapter childAdapter = new HomePageImagesAdapter(context);
+                    holder.gv_images.setAdapter(childAdapter);
+                    holder.gv_images.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            // 因为单张图片时，图片实际大小是自适应的，imageLoader缓存时是按测量尺寸缓存的
+                            DisplayUtil.getNetImageWidthBIHeight();
+                            HomePageImagesAdapter adapter = (HomePageImagesAdapter) parent.getAdapter();
+                            ImagePagerActivity.startImagePagerActivity(context, adapter.getPhones(), position);
+                        }
+                    });
+                }
+                break;
+            case 1:
+                if (holder.ll_video != null) {
+                    holder.ll_video.setVisibility(View.VISIBLE);
+                    holder.item_video_shoot.setImageURI(Uri.parse("http://www.lizhi77.com/uploads/allimg/140930/2-14093010032N14.jpg"));
+                    holder.ll_video.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ToastMgr.show("播放视频");
+                        }
+                    });
+                }
+                break;
+            default:
+                break;
+        }
+
 
         return view;
     }
+
+
+    class Holder {
+        SimpleDraweeView item_preson_icon;
+        SimpleDraweeView item_video_shoot;
+        CardView card;
+        MTextView mtv_content;
+        LinearLayout ll_image_view;
+        LinearLayout ll_video;
+        NoScrollingGridView gv_images;
+    }
+
 }
--- balloon/app/src/main/java/com/ray/balloon/model/bean/RecommendAd.java
+++ balloon/app/src/main/java/com/ray/balloon/model/bean/RecommendAd.java
@@ -1,5 +1,8 @@
 package com.ray.balloon.model.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * 轮播广告
  * @Copyright Copyright © 2014 蓝色互动. All rights reserved.
@@ -7,10 +10,45 @@ package com.ray.balloon.model.bean;
  * @author ZhuFan
  * @Date: 2015-6-2
  */
-public class RecommendAd {
+public class RecommendAd implements Parcelable{
 	public long id;
 	public String imgsrc;
 	public String title;
 	public String createdate;
 	public long productid;
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeLong(this.id);
+		dest.writeString(this.imgsrc);
+		dest.writeString(this.title);
+		dest.writeString(this.createdate);
+		dest.writeLong(this.productid);
+	}
+
+
+	protected RecommendAd(Parcel in) {
+		this.id = in.readLong();
+		this.imgsrc = in.readString();
+		this.title = in.readString();
+		this.createdate = in.readString();
+		this.productid = in.readLong();
+	}
+
+	public static final Creator<RecommendAd> CREATOR = new Creator<RecommendAd>() {
+		@Override
+		public RecommendAd createFromParcel(Parcel source) {
+			return new RecommendAd(source);
+		}
+
+		@Override
+		public RecommendAd[] newArray(int size) {
+			return new RecommendAd[size];
+		}
+	};
 }
